ead023024af672b0947067e8413614b88aefed74
Yusuke Tsuzuku <ytsmiling@gmail.com>
2018-07-21 15:55:31 +0900

    fix bug (unfilled area)

--- solution/default_solution.py
+++ solution/default_solution.py
@@ -15,7 +15,7 @@ class DefaultSolution:
         trace.flip()
         x_dir, y_dir, z_dir = 1, 1, 1
         place = [0, 0, 0]
-        for y in range(resolution - 1):
+        for y in range(resolution):
             for x in range(resolution - 1):
                 for z in range(resolution - 1):
                     if place[1] > 0 and model[place[0], place[1] - 1, place[2]]:
@@ -30,6 +30,8 @@ class DefaultSolution:
             x_dir *= -1
             if place[1] > 0 and model[place[0], place[1] - 1, place[2]]:
                 trace.fill((0, -1, 0))
+            if place[1] + 1 == resolution:
+                break
             trace.s_move((0, y_dir, 0))
             place[1] += y_dir
         while place[0] > 0:
