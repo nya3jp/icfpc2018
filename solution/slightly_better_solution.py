22b916577d467475987b70d0126ca1353b9f7c1c
Yusuke Tsuzuku <ytsmiling@gmail.com>
2018-07-21 21:01:13 +0900

    slight improvement, but contains bug in LMove

--- solution/slightly_better_solution.py
+++ solution/slightly_better_solution.py
@@ -12,38 +12,76 @@ class SlightlyBetterSolution:
         self.filled = self.model.matrix.copy()
         self.filled.fill(False)
 
+    def _is_same_direction(self, a, b):
+        if a[1] == 0 and a[2] == 0 and b[1] == 0 and b[2] == 0:
+            return True
+        if a[0] == 0 and a[1] == 0 and b[0] == 0 and b[1] == 0:
+            return True
+        if a[0] == 0 and a[2] == 0 and b[0] == 0 and b[2] == 0:
+            return True
+        return False
+
+    def _is_sld(self, a, b):
+        if abs(a[0]) > 5 or abs(a[1]) > 5 or abs(a[2]) > 5:
+            return False
+        if abs(b[0]) > 5 or abs(b[1]) > 5 or abs(b[2]) > 5:
+            return False
+        dx, dy, dz = a[0] + b[0], a[1] + b[1], a[2] + b[2]
+        if abs(dx) and abs(dy) and abs(dz):
+            return False
+        if abs(dx) <= 5 and abs(dy) <= 5 and abs(dz) <= 5:
+            return True
+
+    def _is_one_hot(self, a):
+        return ((a[0], a[1]) == (0, 0)) or ((a[1], a[2]) == (0, 0)) or (
+                (a[0], a[2]) == (0, 0))
+
     def _compile(self):
-        # 一番バカなやつ
-        for ld in self.command:
-            dx, dy, dz = ld
-            while abs(dx) + abs(dy) + abs(dz) > 15:
-                if abs(dx) > 0:
-                    self.trace.s_move((15 if dx > 0 else -15, 0, 0))
-                    dx -= 15 if dx > 0 else -15
-                if abs(dy) > 0:
-                    self.trace.s_move((15 if dy > 0 else -15, 0, 0))
-                    dy -= 15 if dy > 0 else -15
-                if abs(dz) > 0:
-                    self.trace.s_move((15 if dz > 0 else -15, 0, 0))
-                    dz -= 15 if dz > 0 else -15
-            self.trace.s_move(ld)
-        self.command.clear()
-        return
-        # dy が動く時にはぶつかりうる
-        x, y, z = self.position
+        # for i in self.command:
+        #     self.trace.s_move(i)
+        # self.command.clear()
+        # return
         i = 0
+        dx, dy, dz = 0, 0, 0
+        first = None
+        x, y, z = self.position
         while i < len(self.command):
-            while i < len(self.command):
-                dx, dy, dz = self.command[i]
-                if dy > 0:
-                    break
-                x += dx
-                y += dy
-                z += dz
+            while (i < len(self.command) and
+                   (self._is_same_direction((dx, dy, dz), self.command[i]) or
+                    self._is_sld((dx, dy, dz), self.command[i]))):
+                dx += self.command[i][0]
+                dy += self.command[i][1]
+                dz += self.command[i][2]
+                if self._is_one_hot((dx, dy, dz)):
+                    first = (dx, dy, dz)
                 i += 1
-            # self.trace.s_move()
-            self.command.clear()
-            self.position = x, y, z
+            if self._is_one_hot((dx, dy, dz)):
+                while abs(dx) + abs(dy) + abs(dz) > 15:
+                    if abs(dx) > 0:
+                        self.trace.s_move((15 if dx > 0 else -15, 0, 0))
+                        dx -= 15 if dx > 0 else -15
+                    if abs(dy) > 0:
+                        self.trace.s_move((0, 15 if dy > 0 else -15, 0))
+                        dy -= 15 if dy > 0 else -15
+                    if abs(dz) > 0:
+                        self.trace.s_move((0, 0, 15 if dz > 0 else -15))
+                        dz -= 15 if dz > 0 else -15
+                else:
+                    if abs(dx) + abs(dy) + abs(dz):
+                        self.trace.s_move((dx, dy, dz))
+                        dx, dy, dz = 0, 0, 0
+            else:
+                if first[0]:
+                    self.trace.l_move((dx, 0, 0), (0, dy, dz))
+                if first[1]:
+                    self.trace.l_move((0, dy, 0), (dx, 0, dz))
+                if first[2]:
+                    self.trace.l_move((0, 0, dz), (dx, dy, 0))
+                first = None
+                dx, dy, dz = 0, 0, 0
+        self.command.clear()
+        self.position = x, y, z
+        return
 
     def simulate(self):
         model = self.model
@@ -82,9 +120,20 @@ class SlightlyBetterSolution:
             # naiveに進む
             dx = 1 if tx > sx else -1
             dz = 1 if tz > sz else -1
+
             # memo: 全てのサイドが空いていることを仮定している
             while x != tx + dx:
-                while z != tz + dz:
+                while z != tz:
+                    # if (0 <= x - dx < resolution and matrix[x - dx, y, z]
+                    #         and not self.filled[x - dx, y, z]):
+                    #     self._compile()
+                    #     trace.fill((-dx, 0, 0))
+                    #     self.filled[x - dx, y, z] = True
+                    # if (0 <= z - dz < resolution and matrix[x, y, z - dz]
+                    #         and not self.filled[x, y, z - dz]):
+                    #     self._compile()
+                    #     trace.fill((0, 0, -dz))
+                    #     self.filled[x, y, z - dz] = True
                     command.append((0, 0, dz))
                     if matrix[x, y, z]:
                         self._compile()
@@ -93,16 +142,33 @@ class SlightlyBetterSolution:
                     z += dz
                 dz *= -1
                 sz, tz = tz, sz
-                command.append((dx, 0, 0))
-                if matrix[x, y, z]:
-                    self._compile()
-                    trace.fill((-dx, 0, 0))
-                    self.filled[x, y, z] = True
-                x += dx
+                # if (0 <= x - dx < resolution and matrix[x - dx, y, z]
+                #         and not self.filled[x - dx, y, z]):
+                #     self._compile()
+                #     trace.fill((-dx, 0, 0))
+                #     self.filled[x - dx, y, z] = True
+                # if (0 <= z - dz < resolution and matrix[x, y, z - dz]
+                #         and not self.filled[x, y, z - dz]):
+                #     self._compile()
+                #     trace.fill((0, 0, -dz))
+                #     self.filled[x, y, z - dz] = True
+                if x != tx:
+                    command.append((dx, 0, 0))
+                    if matrix[x, y, z]:
+                        self._compile()
+                        trace.fill((-dx, 0, 0))
+                        self.filled[x, y, z] = True
+                    x += dx
+                else:
+                    break
 
             if not y == resolution - 1:
                 command.append((0, 1, 0))
                 y += 1
+                if matrix[x, y - 1, z]:
+                    self._compile()
+                    trace.fill((0, -1, 0))
+                    self.filled[x, y - 1, z] = True
             if not matrix[:, y:, :].any():
                 break
 
