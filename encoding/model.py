bc84cf34dae3028422ec62b05a361ea180273f39
Yusuke Tsuzuku <ytsmiling@gmail.com>
2018-07-21 11:33:18 +0900

    add model encoder/decoder

---
+++ encoding/model.py
@@ -0,0 +1,46 @@
+import math
+import numpy as np
+
+
+class Model:
+    """Init with model file file path and accessible like numpy array
+    Model.decode_model(path) method returns bool matrix representing model
+    Model.encode_model(path, data) method dumps matrix to .mdl file
+
+    """
+
+    def __init__(self, path):
+        self.matrix = Model.decode_model(path)
+        self.resolution = self.matrix.shape[0]
+
+    def __getitem__(self, item):
+        return self.matrix[item]
+
+    @classmethod
+    def decode_model(cls, path):
+        with open(path, 'rb') as bf:
+            bf.seek(0)
+            data = np.frombuffer(bf.read(), dtype=np.uint8)
+        resolution = data[0]
+        ret = np.empty((resolution,) * 3, dtype=bool)
+        for i in range(resolution ** 3):
+            x = i // resolution ** 2
+            y = (i % resolution ** 2) // resolution
+            z = i % resolution
+            ret[x, y, z] = bool(data[i // 8 + 1] & (1 << (7 - (i % 8))))
+        return ret
+
+    @classmethod
+    def encode_model(cls, path, data):
+        size = int(math.ceil((data.shape[0] ** 3 + 1) / 8) + .1)
+        ret = np.zeros(size, dtype=np.uint8)
+        resolution = data.shape[0]
+        ret[0] = resolution
+        for i in range(data.shape[0] ** 3):
+            x = i // resolution ** 2
+            y = (i % resolution ** 2) // resolution
+            z = i % resolution
+            ret[i // 8 + 1] += (1 << (7 - (i % 8))) if data[x, y, z] else 0
+        encoded = ret.tobytes()
+        with open(path, 'wb') as bf:
+            bf.write(encoded)
