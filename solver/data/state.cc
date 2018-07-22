633e010c9bb2cf0cd6838d5941c88eab8b2f02a6
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2018-07-22 18:30:46 +0900

    Fix a bug in state.cc

--- solver/data/state.cc
+++ solver/data/state.cc
@@ -8,13 +8,13 @@ constexpr int NUM_BOTS = 40;
 
 BotState::BotState(int id, uint64_t seeds, Point position)
     : id_(id), seeds_(seeds), position_(position) {
-  CHECK((seeds & (static_cast<uint64_t>(1) << id)) != 0);
+  CHECK((seeds & (static_cast<uint64_t>(1) << id)) == 0);
 }
 
 // static
 FieldState FieldState::FromModels(Matrix source, Matrix target) {
   std::map<int, BotState> bots;
-  BotState bot(1, (static_cast<uint64_t>(1) << NUM_BOTS) - 2, Point(0, 0, 0));
-  bots.emplace(1, std::move(bot));
+  BotState bot(0, (static_cast<uint64_t>(1) << NUM_BOTS) - 2, Point(0, 0, 0));
+  bots.emplace(0, std::move(bot));
   return FieldState(std::move(target), std::move(source), std::move(bots));
 }
