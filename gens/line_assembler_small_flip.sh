e00450de00a4f92f9e996f4fcf498fa63310564a
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2018-07-23 20:15:01 +0900

    Change division

--- gens/line_assembler_small_flip.sh
+++ gens/line_assembler_small_flip.sh
@@ -18,8 +18,8 @@ done
 for i in $(seq -f %03g 1 30); do
   bazel-bin/solver/solver \
       --line_assembler_flip_xz \
-      --line_assembler_x_divs 2 \
-      --line_assembler_z_divs 1 \
+      --line_assembler_x_divs 1 \
+      --line_assembler_z_divs 2 \
       --impl line_assembler \
       --target data/models/FA${i}_tgt.mdl \
       --output a.nbt
